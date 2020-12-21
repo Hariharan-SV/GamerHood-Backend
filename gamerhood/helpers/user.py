5d363952e1794c4648b7f0e2d09965148022c9bf
Hariharan-SV <harikumar20112000@gmail.com>
2020-12-21 18:20:04 +0530

    helpers/user: Add token required validator

---
+++ gamerhood/helpers/user.py
@@ -0,0 +1,19 @@
+from functools import wraps
+from flask import request
+import jwt, os
+
+def token_required(f):
+  @wraps(f)
+  def decorator(*args, **kwargs):
+    token = None
+    authorized = False
+    if 'x-auth-token' in request.headers:
+      token = request.headers['x-auth-token']
+    if token is not None:
+      try:
+        jwt.decode(token, os.environ.get('key'))
+        authorized = True
+      except jwt.ExpiredSignatureError or jwt.InvalidTokenError:
+        authorized = False
+    return f(authorized,*args, **kwargs)            
+  return decorator
