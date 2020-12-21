d7ea4bee05e43fe1e86cc0ca6612952352e9bb0a
Hariharan-SV <harikumar20112000@gmail.com>
2020-12-21 18:20:38 +0530

    controllers/game: Add search keyword function

--- gamerhood/controllers/game.py
+++ gamerhood/controllers/game.py
@@ -1,17 +1,15 @@
-from flask import Blueprint, request, session
+from flask import Blueprint
+from flask.helpers import make_response
+from flask.json import jsonify
+from gamerhood.helpers.user import token_required
+from gamerhood.services.game import get_results_from_keyword
 
 game = Blueprint('game', __name__, url_prefix="/game")
 
-@game.route('/session')
-def getSession():
-  try:
-    if(session['email'] is not None):
-      return session['email']
-    else:
-      return 'not defined'
-  except KeyError:
-    return 'Key error'
-
-@game.route('/')
-def getDashboard():
-  return 'Game'
+@game.route('/search/<keyword>')
+@token_required
+def get_matching_games(authorized:bool,keyword:str):
+  if not authorized:
+    make_response(jsonify({"status": 0, "message": "Token is missing !!"}), 401)
+  results = get_results_from_keyword(keyword)
+  return make_response(jsonify({"status":1,"data":results[:10]}),200)
