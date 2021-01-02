c7fd72903794bd417c7abb9319bf87076477e102
Hariharan-SV <harikumar20112000@gmail.com>
2021-01-02 12:15:35 +0530

    Add change password function

--- gamerhood/controllers/user.py
+++ gamerhood/controllers/user.py
@@ -1,37 +1,18 @@
+from gamerhood.controllers.auth import get_session
 from flask import Blueprint, request, session, jsonify
-from gamerhood.services.user import getSessionDetails
+from flask.helpers import make_response
+from gamerhood.helpers.user import token_required
 
 user = Blueprint('user', __name__, url_prefix="/user")
 
-@user.route('/session/mail',methods=['GET'])
-def getMail():
-  print(session)
-  if('email' in session):
-    return jsonify(session['email'])
-  else:
-    return{"msg":"email not in session"}
-
-@user.route('/session/data',methods=['GET'])
-def getSession():
-  print(session)
-  if('data' in session):
-    return jsonify(session['data'])
-  elif('email' in session):
-    session['data'] = getSessionDetails(session['email'])
-    print("data returned is ",session)
-    return jsonify(session['data'])
-  else:
-    return{"msg":"email not in session"}
-  
-
-@user.route('/clearSession')
-def clearSession():
-  try:
-    session.clear()
-    return {"msg":"Cleared"}
-  except:
-    return {"msg":"Not Cleared"}
-
-@user.route('/dashboard')
-def getDashboard():
-  return 'Dashboard'
+@user.route('/change-password',methods=['POST'])
+@token_required
+def change_password(authorized:bool):
+  if not authorized:
+    return make_response({"status": 0, "message": "Token not found !"},401)
+  data = request.get_json()
+  if(data['password']!=data['retype-password']):
+    return make_response({"status": 0, "message": "Passwords Mismatch !"},200)
+  user_data = get_session()
+  print(user_data)
+  return make_response({"status": 1, "message": "Checking !"},200)
