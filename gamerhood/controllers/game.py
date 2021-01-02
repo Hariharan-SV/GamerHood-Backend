18ae8304dead6bda7d13f0773109e39e53de8f0a
Hariharan-SV <harikumar20112000@gmail.com>
2021-01-02 12:15:15 +0530

    Add route to view game details

--- gamerhood/controllers/game.py
+++ gamerhood/controllers/game.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 from flask.helpers import make_response
 from flask.json import jsonify
 from gamerhood.helpers.user import token_required
-from gamerhood.services.game import get_results_from_keyword
+from gamerhood.services.game import get_results_from_keyword,get_game_from_id
 
 game = Blueprint('game', __name__, url_prefix="/game")
 
@@ -13,3 +13,13 @@ def get_matching_games(authorized:bool,keyword:str):
     make_response(jsonify({"status": 0, "message": "Token is missing !!"}), 401)
   results = get_results_from_keyword(keyword)
   return make_response(jsonify({"status":1,"data":results[:10]}),200)
+
+@game.route('/details/<id>')
+@token_required
+def get_game_details(authorized:bool,id:int):
+  if not authorized:
+    make_response(jsonify({"status": 0, "message": "Token is missing !!"}), 401)
+  results = get_game_from_id(id)
+  if results is None:
+    make_response(jsonify({"status": 0, "message": "No game found!"}), 200)
+  return make_response(jsonify({"status":1,"data":results}),200)
