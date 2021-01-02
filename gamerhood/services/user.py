67a57a1d6a0e7ebc2bb8090000b67787d50cf88c
Hariharan-SV <harikumar20112000@gmail.com>
2021-01-03 00:09:22 +0530

    Add service to get user details

--- gamerhood/services/user.py
+++ gamerhood/services/user.py
@@ -1,5 +1,25 @@
-# importing module 
 from pymongo import MongoClient
+import os
 
-def set_password():
+def add_game_to_user_searches(email:str,record:dict):
+  client = MongoClient(os.environ.get('database_url'))
+  db = client['steam_data']
+  collection = db.users
+  try:
+    collection.update_one({"email":email},{"$push":{"searches":record}})
+  except:
+    return 0
   return 1
+
+def get_user_details(email:str):
+  val = None
+  client = MongoClient(os.environ.get('database_url')) 
+  db = client['steam_data'] 
+  collection = db.users
+  try:
+    val = collection.find_one({'email': email})
+    val.pop('password',None)
+    val.pop('_id',None)
+  except:
+    return 0, None
+  return 1, val
