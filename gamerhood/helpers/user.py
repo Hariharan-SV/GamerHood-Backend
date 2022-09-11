fc52bf34f5d43fb536dccfeab08f86f1aa988ee9
Hariharan S V <46956177+Hariharan-SV@users.noreply.github.com>
2022-09-11 14:15:43 +0530

    Update user.py

--- gamerhood/helpers/user.py
+++ gamerhood/helpers/user.py
@@ -13,7 +13,7 @@ def token_required(f):
       try:
         jwt.decode(token, os.environ.get('key'))
         authorized = True
-      except [jwt.ExpiredSignatureError,jwt.InvalidTokenError] as error:
+      except (jwt.ExpiredSignatureError,jwt.InvalidTokenError) as error:
         authorized = False
     return f(authorized,*args, **kwargs)            
   return decorator
